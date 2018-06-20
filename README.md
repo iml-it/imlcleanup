5a01b999ecc74c8f74e067b5cfa847b26425e500
Hahn <hahn@campus.unibe.ch>
2018-06-20 08:34:59 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,144 @@
+
+
+# IML CLEANUP #
+
+
+
+## Introduction ##
+
+For deleting files older N days in a start folder we oftened used several customized shell scripts using a find command.
+
+The IML CLEANUP simply makes a splitting of logic and configuration data.
+It is easier to generate a small config file (especially if you use automation tools like puppet, ansible or chef) then handling several cleanup scripts.
+
+How to get it work:
+
+* In the /etc/imlcleanup.d/ you can put (as many) configfiles you want
+* a shell script loops over all config files and performs the actions of all conf files.
+* you additionally need to create a cronjob to execute this script regulary (i.e. once per day)
+
+
+
+## License ##
+
+GNU General Public License version 3
+
+Copyright (C) 2018  University of Bern; Institute of Medical Education
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	
+
+## Installation ##
+
+### Download ###
+
+1. Download the software and extract it to a folder - i.e. /opt/imlcleanup/
+2. create new directory /etc/imlcleanup.d/ OR move directory ./etc/imlcleanup.d/ to /etc
+
+### Security/ Hardening ###
+
+We use the script on a webserver where several services and applications run. Developers and some users sometimes have shell access. The cleanup runs as root.
+
+To prevent that users or applications generate a config file with destructive information we protect the files that they are accessible by root only.
+
+For the installation directory
+
+```shell
+# chown -R root:root /opt/imlcleanup/
+# chmod 0700 /opt/imlcleanup/imlcleanup.sh
+```
+
+For the config files:
+```shell
+# chown -R root:root /etc/imlcleanup.d/
+# chmod 0700 /etc/imlcleanup.d/
+# chmod 0600 /etc/imlcleanup.d/*
+```
+
+
+
+## Command Line Parameters ##
+
+### -h: Help ##
+
+The option -h shows a help:
+
+```shell
+Syntax: imlcleanup.sh [options]
+    -d             dryrun;  show results but no deletion
+    -f [filename]  process given conf file instead of all in /etc/imlcleanup.d
+    -h             this help
+```
+
+### -f [filename]: Handle a single config file ##
+
+By default all files in /etc/imlcleanup.d/*.conf will be executed.
+To specify a single file - if you should have several configs there - you can use -f parameter.
+This is useful with -d (dryrun).
+
+### -d: Dryrun ##
+
+Without parameter or -f [filename] delete actions will be performed.
+For testing purposes you can use a dryrun to see the found files and empty directories.
+
+
+
+## Configuration Files ##
+
+First of all an example:
+
+```
+dir = /your/starting/path
+filemask = *.log,*.gz
+maxage = 180
+maxdepth =
+deleteemptydirs = 1
+runas = root
+```
+
+The syntax is:
+* Variable starts in first char (no starting spaces)
+* followed by space + "=" + space
+* followed by value without any quoting
+
+
+
+| Variable        | Description                       | Required                                       |
+| ---             | ---                               | ---                                            |
+| dir             | your starting path for the scan   | Y                                              |
+| filemask        | filemask(s)                       | Y                                              |
+| maxage          | max age of a file in days (mtime) | Y                                              |
+| maxdepth        | max recursion level               | n (empty is default = no restriction)          |
+| deleteemptydirs | flag (0 or 1)                     | n (0 is default = do NOT delete empty folders) |
+| runas           | unix user to run                  | n (root is default)                            |
+
+For _dir_ and _filemask_ you can set multiple values: use a comma "," to separate them.
+
+Make a copy of the example file in /etc/imlcleanup.d/ and name it [your-task].conf.
+
+
+
+
+## Cronjob ##
+
+In a cron.d config file or crontab of root you need to start the cleanup script for processing all configs at once.
+
+As an example a file /etc/cron.d/imlcleanup can be one line to start it daily at 4:12 am.
+
+
+```shell
+$ cat /etc/cron.d/imlcleanup
+12 4 * * *  root  /opt/imlcleanup/imlcleanup.sh >/var/log/last_cleanup.log 2>&1 
+```
---
+++ imlcleanup.sh
@@ -0,0 +1,187 @@
+#!/bin/bash
+# ======================================================================
+#
+# IML CLEANUP SCRIPT
+#
+# remove outdated files and remove empty directories
+# by reading config files from /etc/imlcleanup.d
+#
+# ----------------------------------------------------------------------
+# 2018-06-19  <axel.hahn@iml.unibe.ch> v1.1  parameter support
+# 2018-06-18  <axel.hahn@iml.unibe.ch> v1.0
+# ======================================================================
+
+# ----------------------------------------------------------------------
+# VARS
+# ----------------------------------------------------------------------
+
+confdir=/etc/imlcleanup.d
+sConfFiles="$confdir/*.conf"
+tmperrorfile=/tmp/imlcleanup.err
+typeset -i bDryrun=0
+
+
+cmdFiles="rm -f"
+cmdDirs="rmdir"
+
+# ----------------------------------------------------------------------
+# FUNCTIONS
+# ----------------------------------------------------------------------
+
+# helper function - read a variable
+# function requires $mycfgfile that is used in the loop of main part
+function getValue(){
+        cat "${mycfgfile}" | grep "^$1\ = " | cut -f 3- -d " "
+}
+
+function usage(){
+        echo Syntax: `basename $0` [options]
+        echo "    -d             dryrun;  show results but no deletion"
+        echo "    -f [filename]  process given conf file instead of all in $confdir"
+        echo "    -h             this help"
+        echo
+}
+
+# ----------------------------------------------------------------------
+# MAIN
+# ----------------------------------------------------------------------
+cat <<endofheader
+ _______ _______ _____        ______ __
+|_     _|   |   |     |_     |      |  |.-----.---.-.-----.--.--.-----.
+ _|   |_|       |       |    |   ---|  ||  -__|  _  |     |  |  |  _  |
+|_______|__|_|__|_______|    |______|__||_____|___._|__|__|_____|   __|
+                                                                |__|
+endofheader
+
+while getopts ":d :f: :h" o; do
+    case "${o}" in
+        d)
+            bDryrun=1
+            cmdFiles="ls -l"
+            cmdDirs="ls -ld"
+            echo "INFO: dryrun is active. Just showing the files; no deletion"
+            ;;
+        f)
+            sConfFiles=${OPTARG}
+            ls -l $sConfFiles  >/dev/null 2>&1
+            if [ $? -ne 0 ]; then
+                echo "ERROR: -f [$sConfFiles]"
+                echo "           ^"
+                echo "           |"
+                echo "           +-- the given filename seems to be wrong"
+                echo
+                exit 1
+            fi
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        ?)
+            echo "ERROR: Invalid option: -$OPTARG" >&2
+            usage
+            exit 1
+            ;;
+        *)
+            # usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+rm -f $tmperrorfile 2>/dev/null
+
+ls -l $sConfFiles  >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+        echo SKIP nothing to do.
+        echo The directory $confdir does not exist or has no *conf files. But this is no error.
+        echo
+        exit 0
+fi
+
+echo "INFO: reading configs from $sConfFiles"
+ls -1 $sConfFiles | while read mycfgfile
+do
+        echo
+        echo ">>>>>>>>>> PROCESSING $mycfgfile ..." | tee -a $tmperrorfile
+        echo
+        # cat $mycfgfile
+        mydirs=`getValue dir`
+        filemasks=`getValue filemask`
+        typeset -i iAge=`getValue maxage`
+        typeset -i iMaxDepth=`getValue maxdepth`
+
+        bDeleteemptydirs=`getValue deleteemptydirs`
+        sRunAs=`getValue runas`
+
+        # ----- checks
+        if [ -z "$mydirs" -o -z "$filemasks" -o -z "$iAge" ]; then
+                echo ERROR: invalid config file $mycfgfile        | tee -a $tmperrorfile
+                echo dir, filemask and maxage are required values | tee -a $tmperrorfile
+                echo
+        else
+                # maxdepth for scanning
+                depthParam=
+                if [ $iMaxDepth -gt 0 ]; then
+                        depthParam="-maxdepth $iMaxDepth"
+                fi
+
+                # split filemasks by "," and add -name params
+                #   filemask = *.log,*.gz
+                #   ... results in
+                #   \( -name "*.log" -o -name "*.gz" \)
+                maskParam="`echo "${filemasks}" | sed 's#,#\" -o -name \"#g'`"
+
+                if [ $? -ne 0 ]; then
+                        echo ERROR: user [$sRunAs] is unknown | tee -a $tmperrorfile
+                else
+
+                        # run as a given user
+                        suPrefix="su - $sRunAs -c "
+
+                        # split mydirs by "," and add loop
+                        echo $mydirs | sed "s#,#\\n#g" | while read mydir
+                        do
+                                echo "-----> $mydir" | tee -a $tmperrorfile
+                                echo
+                                if [ ! -d "$mydir" ]; then
+                                        echo ERROR: $mydir does not exist | tee -a $tmperrorfile
+                                        echo Skipping this file           | tee -a $tmperrorfile
+                                else
+                                        echo "[1] cleanup ${filemasks} older ${iAge} days" | tee -a $tmperrorfile
+                                        set -vx
+                                        $suPrefix "find ${mydir} $depthParam -type f \( -name \"${maskParam}\" \) -mtime +${iAge} -print -exec $cmdFiles {} \;" \
+                                                2>>$tmperrorfile \
+                                                || echo ERROR: cleanup of files failed \
+                                                | tee -a $tmperrorfile
+                                        set +vx
+
+                                        if [ "$bDeleteemptydirs" = "1" ]; then
+                                                echo [2] Delete empty dirs | tee -a $tmperrorfile
+                                                set -vx
+                                                ${suPrefix} "find ${mydir} ${depthParam} -depth -type d -empty -print -exec $cmdDirs {} \;" \
+                                                        2>>$tmperrorfile \
+                                                        || echo ERROR: cleanup of empty dirs failed \
+                                                        | tee -a $tmperrorfile
+                                                set +vx
+                                        else
+                                                echo [2] SKIP: no scan for empty directories | tee -a $tmperrorfile
+                                        fi
+                                fi
+                                echo
+                        done
+                fi
+        fi
+done
+
+echo ---------- DONE
+typeset -i iErrors=`grep "ERROR" $tmperrorfile | wc -l`
+echo ERRORS: $iErrors
+echo
+if [ $iErrors -gt 0 ]; then
+  echo places of found errors:
+  cat $tmperrorfile
+  echo
+fi
+rm -f $tmperrorfile
+exit $iErrors
